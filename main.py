ccc0b55e5954afde2fb38a6e0aa7fdee37a3c4a0
muhammadmada <muhammad.mada.ibrahim@mail.ugm.ac.id>
2023-11-05 11:58:26 +0700

    Update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from preprocess import preprocess_images
 from Loader import load_dataset_styler
 from autoencoder import Autoencoder
+from Callbacks import CustomCallback
 import numpy as np
 from keras.optimizers import Adam
 
@@ -11,39 +12,28 @@ X_train_styler = load_dataset_styler(styler_path, exts)
 
 image_paths = [path for _, class_images in X_train_styler for path in class_images]
 
-
 X_train_styler = preprocess_images(image_paths)
 X_train_styler = [np.array(image) for image in X_train_styler]
 
 input_dim = (256, 256, 3)
 encoding_dim = (32, 32, 128)
 learning_rate = 0.0001
-adam_optimizer = Adam(learning_rate=learning_rate)
-autoencoder = Autoencoder(input_dim, encoding_dim)
-autoencoder.autoencoder.compile(optimizer=adam_optimizer, loss='mean_squared_error')
 
-epochs = 32
+autoencoder = Autoencoder(input_dim, encoding_dim, learning_rate)
+
+epochs = 10
 batch_size = 128
+validation_split = 0.2
 
-for epoch in range(epochs):
-    print(f"Epoch {epoch + 1}/{epochs}")
+custom_callback = CustomCallback()
 
-    total_loss = 0.0
-    num_batches = len(X_train_styler) // batch_size
+optimizer = Adam(learning_rate=learning_rate)
+autoencoder.autoencoder.compile(optimizer=optimizer, loss='mean_squared_error')
 
-    for i in range(0, len(X_train_styler), batch_size):
-        batch_X = X_train_styler[i:i + batch_size]
-        batch_loss = 0
+autoencoder.autoencoder.fit(X_train_styler, X_train_styler,
+    batch_size=batch_size, epochs=epochs, validation_split=0.2,
+    callbacks=custom_callback, verbose=2)
 
-        for image in batch_X:
-            image = image.reshape(1, 256, 256, 3)
-
-            loss = autoencoder.autoencoder.train_on_batch(image, image)
-            batch_loss += loss
-
-        total_loss += batch_loss / len(batch_X)
-
-    average_loss = total_loss / num_batches
-    print(f"Epoch {epoch + 1} - Average Loss: {average_loss:.4f}")
 autoencoder.autoencoder.save('/mnt/d/Documents/Coolyeah/DL/models/autoencoder.h5')
+
 print("Autoencoder training completed.")
